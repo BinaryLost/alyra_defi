bfba71af788c01536e88a4973f29a92125dcc361
mikesolg <76155783+mikesolg@users.noreply.github.com>
2022-08-06 15:23:45 +0200

    Update 1_staking_migration.js

--- truffle/migrations/1_staking_migration.js
+++ truffle/migrations/1_staking_migration.js
@@ -9,13 +9,13 @@ module.exports = async function (deployer) {
   const token = await(RewardToken.deployed());
   console.log('Reward Token Address', RewardToken.address);
   
-  // Create staking contract with a reward rate of 10/sec  
-  await deployer.deploy(Staking, 10, RewardToken.address, RewardToken.address);
+  // Create staking contract with a reward rate of 10/sec  / stake RINKEBY LINK token 
+  await deployer.deploy(Staking, 10, "0x01BE23585060835E02B77ef475b0Cc51aA1e0709", RewardToken.address);
   const stakingContractInstance = await(Staking.deployed());
   console.log('Staking Contract Address', Staking.address);
 
   // Now transfer initial supply of reward token to staking contract
-  await token.transfer(Staking.address , new BN(initialSupply-1000000).mul(new BN("1000000000000000000")));
+  await token.transfer(Staking.address , new BN(initialSupply).mul(new BN("1000000000000000000")));
 
   //await(rewardTokenInstance.transfer(Staking.address, 1000000000000000000));//new BN(""+(initialSupply/2)+"10**18")));
   //let j = await rewardTokenInstance.balanceOf(Staking.address);
