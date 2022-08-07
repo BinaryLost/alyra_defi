d2d70a6608e96d937f15be468a8395f13fdab9de
MikeSolg <MikeSolg@MikeSolg>
2022-08-07 11:08:40 +0200

    correction addresses migration

--- truffle/migrations/1_staking_migration.js
+++ truffle/migrations/1_staking_migration.js
@@ -2,22 +2,18 @@ const Staking = artifacts.require("Staking.sol");
 const RewardToken = artifacts.require("RewardToken.sol");
 const { BN } = require("@openzeppelin/test-helpers");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, addresses) {
   // Create reward token with 10**9 initial supply
   const initialSupply = 10**9;
   await deployer.deploy(RewardToken, initialSupply);
   const token = await(RewardToken.deployed());
-  console.log('Reward Token Address', RewardToken.address);
+  console.log('Reward Token Address', token.address);
   
   // Create staking contract with a reward rate of 10/sec  / stake RINKEBY LINK token 
   await deployer.deploy(Staking, 10, "0x01BE23585060835E02B77ef475b0Cc51aA1e0709", RewardToken.address);
   const stakingContractInstance = await(Staking.deployed());
-  console.log('Staking Contract Address', Staking.address);
+  console.log('Staking Contract Address', stakingContractInstance.address);
 
   // Now transfer initial supply of reward token to staking contract
   await token.transfer(Staking.address , new BN(initialSupply).mul(new BN("1000000000000000000")));
-
-  //await(rewardTokenInstance.transfer(Staking.address, 1000000000000000000));//new BN(""+(initialSupply/2)+"10**18")));
-  //let j = await rewardTokenInstance.balanceOf(Staking.address);
-  //console.log("balance staking", j)
 };
